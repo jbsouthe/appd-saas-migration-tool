ab5fdc6a989c3c596733b02e07d2a619db74e9ce
jbsouthe <jbsouthe@github.com>
2022-03-23 16:51:37 -0600

    fixed an issue where sometimes the numeric data is larger than a Long.MAX_VALUE, so just going straight to String instead

--- src/main/java/com/cisco/josouthe/controller/dbdata/DatabaseMetricValue.java
+++ src/main/java/com/cisco/josouthe/controller/dbdata/DatabaseMetricValue.java
@@ -7,7 +7,8 @@ import java.sql.SQLException;
 
 public class DatabaseMetricValue {
     //Level 1: metricdata_hour_agg_app
-    public Long ts_min, metric_id, application_id, group_count_val, count_val, sum_val, min_val, max_val, cur_val, weight_value_square, weight_value, rollup_type, cluster_rollup_type;
+    public Long ts_min, metric_id, application_id;
+    public String group_count_val, count_val, sum_val, min_val, max_val, cur_val, weight_value_square, weight_value, rollup_type, cluster_rollup_type;
     //Level 2: metricdata_hour_agg
     public Long application_component_instance_id;
     //Level 3: metricdata_hour
@@ -17,16 +18,16 @@ public class DatabaseMetricValue {
     public DatabaseMetricValue(String type, ResultSet resultSet) throws SQLException, BadDataException {
         ts_min = resultSet.getLong("ts_min");
         metric_id = resultSet.getLong("metric_id");
-        group_count_val = resultSet.getLong("group_count_val");
-        count_val = resultSet.getLong("count_val");
-        sum_val = resultSet.getLong("sum_val");
-        min_val = resultSet.getLong("min_val");
-        max_val = resultSet.getLong("max_val");
-        cur_val = resultSet.getLong("cur_val");
-        weight_value_square = resultSet.getLong("weight_value_square");
-        weight_value = resultSet.getLong("weight_value");
-        rollup_type = resultSet.getLong("rollup_type");
-        cluster_rollup_type = resultSet.getLong("cluster_rollup_type");
+        group_count_val = resultSet.getString("group_count_val");
+        count_val = resultSet.getString("count_val");
+        sum_val = resultSet.getString("sum_val");
+        min_val = resultSet.getString("min_val");
+        max_val = resultSet.getString("max_val");
+        cur_val = resultSet.getString("cur_val");
+        weight_value_square = resultSet.getString("weight_value_square");
+        weight_value = resultSet.getString("weight_value");
+        rollup_type = resultSet.getString("rollup_type");
+        cluster_rollup_type = resultSet.getString("cluster_rollup_type");
         this.type=type;
         switch(type) {
             case "node":
